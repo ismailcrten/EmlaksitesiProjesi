b4c816dcedac34a4030b1cc004815ebdc79a78af
cortenismail@gmail.com <cortenismail@gmail.com>
2023-10-22 01:22:50 +0300

    veri tabanı ve emlak sayfası eklendi

--- emlakkiralamaprojesi/settings.py
+++ emlakkiralamaprojesi/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'emlak.apps.EmlakConfig',
     'home.apps.HomeConfig',
     'django.contrib.admin',
     'django.contrib.auth',
